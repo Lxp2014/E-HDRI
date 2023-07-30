9ff14db230edf7ff7e52e695d7e002b6713b803b
lixiaopeng123456 <54618126+lixiaopeng123456@users.noreply.github.com>
2023-07-30 21:03:12 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python test.py --test_input ./Datasets/SE_dynamic/ --outputs_dir output/ --which_epoch 200 --how_many 20
+CUDA_VISIBLE_DEVICES=0 python test.py --test_input ./Datasets/SE_static/ --outputs_dir output/ --which_epoch 200 --how_many 20
